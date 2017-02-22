6b0d7f67abd37994b0fba463f9c550c90e1fdee7
Tosby <o2jose43@gmail.com>
2017-02-22 10:02:37 +0300

    Added the review proposal page and supporting files

--- pages/default1.php
+++ pages/default1.php
@@ -38,6 +38,7 @@ and open the template in the editor.
                 <div class="collapse navbar-collapse navHeaderCollapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li class="active"><a href="#">Home</a></li>
+                        <li><a href="Review.html" target="-blank">Review Proposals</a></li>
                         <li><a href="#">Contact us</a></li>
                     </ul>
                 </div>
